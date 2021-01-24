f85cc7b7309f1d3569279591c7ae05d2aa3a2981
OxGsOuRcE <69318585+oxgarmy@users.noreply.github.com>
2021-01-24 21:22:02 +0300

    Update Earth.py

--- userbot/plugins/Earth.py
+++ userbot/plugins/Earth.py
@@ -10,7 +10,7 @@ from collections import deque
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
+	deq = deque(list("♥️🧡💚❤💙💜🤎🖤"))
 	for _ in range(48):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
